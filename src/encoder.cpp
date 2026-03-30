81db17bab76154edd06a687644001cd575796fb5
Preet2099x <preet2099x@gmail.com>
2026-03-30 11:50:04 +0530

    Initial commit

---
+++ src/encoder.cpp
@@ -0,0 +1,33 @@
+#include <var.h>
+#include <Arduino.h>
+
+
+void updateEncoder_L() {
+  int MSB = digitalRead(encoderPin_1_L); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin_2_L); //LSB = least significant bit
+
+  int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded_L << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue_L ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue_L --;
+
+  lastEncoded_L = encoded; //store this value for next time
+  lastMSB_L = MSB;
+  lastLSB_L = LSB;
+}
+
+void updateEncoder_R() {
+  int MSB = digitalRead(encoderPin_1_R); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin_2_R); //LSB = least significant bit
+
+  int encoded = (MSB << 1) | LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded_R << 2) | encoded; //adding it to the previous encoded value
+
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue_R ++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue_R --;
+
+  lastEncoded_R = encoded; //store this value for next time
+  lastMSB_R = MSB;
+  lastLSB_R = LSB;
+}
